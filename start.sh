e699c7ea778a1bb3de04134e391abbafacc5973b
svallero <svallero@to.infn.it>
2016-06-01 17:25:28 +0200

    Change oneadmin password in start-up script.

--- start.sh
+++ start.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
+
+# New password can be passed at runtime
+echo oneadmin:${ONE_PASSWORD} > ${ONE_AUTH}
+
+# start oned and scheduler
+# (mh this is not microservices style)
 one start
+
+# process to keep the container alive ;)
 tail -f /var/log/one/oned.log
